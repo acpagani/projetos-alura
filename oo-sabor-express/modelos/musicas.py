5ce948a48fc361efb34ff3143994c93f4e1a3fb1
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-08-25 00:00:12 -0300

    python OO
    aulas alura

---
+++ oo-sabor-express/modelos/musicas.py
@@ -0,0 +1,11 @@
+class Musica:
+    def __init__(self, nome='', artista='', duracao=0):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+
+
+musica1 = Musica()
+musica1.nome = "Adada"
+musica1.artista = 'adada'
+musica1.duracao = 234
