99e0bdaf0ac7e5b43586a70589802174a8256fa0
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2025-03-04 18:37:05 -0300

    feat/alura-oo

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -4,6 +4,7 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de melancia', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Pãozinho', 2.0, 'O melhor pão da cidade')
 restaurante_praca.add_cardapio(bebida_suco)
 restaurante_praca.add_cardapio(prato_paozinho)
